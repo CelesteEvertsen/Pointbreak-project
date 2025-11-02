587c4d313712700f5c255462465beaf177455163
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-11-02 17:02:16 +0100

    typeof

--- src/app/components/CourseSection/CourseSection.tsx
+++ src/app/components/CourseSection/CourseSection.tsx
@@ -1,8 +1,56 @@
-import type { Main } from "@/payload-types";
+import style from './CourseSection.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
-type Props ={
-    data: Main[]
+type CourseImage = {
+  id: number
+  alt?: string
+  url: string
+  width: number
+  height: number
+}
+type CourseSectionProps = {
+  slug: string |null | undefined
+  CourseImage: CourseImage | null
+  CoursePrice: string
+  CourseName: string
+  AmoutOfPeople: number
+  Description: string
+  Button: string
+}
+type CourseProps = {
+  data: CourseSectionProps[]
 }
 
+export default function CourseSection ({data}: CourseProps){
+    return(
+         <div className={style.container}>
+      <section className={style.card}>
+        {data.map((info) => (
+          <section key={info.Description}>
+            <div className={style.imageWrapper}>
+              <Image
+                src={info.CourseImage?.url || '/fallback.jpg'}
+                alt={info.CourseImage?.alt || info.CourseName}
+                width={200}
+                height={200}
+              />
+            </div>
+
+            <div className={style.content}>
+              <h2 className={style.name}>{info.CourseName}</h2>
+              <p className={style.price}>{info.CoursePrice}</p>
+              <p className={style.people}> {info.AmoutOfPeople} personer</p>
+              <p className={style.description}>{info.Description}</p>
+              <Link href={`home/${info.slug}`}>
+                <button type="button" className={style.button}>
+                  {info.Button}
+                </button>
+              </Link>
+            </div>
+          </section>
+        ))}
+      </section>
+    </div>
+    )
+}
---
+++ src/renderBlocks/RendeBlocks.tsx
@@ -0,0 +1,46 @@
+import CourseSection from '@/app/components/CourseSection/CourseSection'
+import { CourseBlock } from '@/payload-types'
+import { Fragment } from 'react'
+
+type CustomBlock = CourseBlock
+type RenderBlocksProps ={
+    blocks: CustomBlock[] | null |undefined
+}
+
+export default function RenderBlocks({blocks}:RenderBlocksProps){
+    if(!blocks || !Array.isArray(blocks)){
+        return null
+    }
+    return(
+        <>
+ {blocks.map((block) => {
+        const courseImage =
+          typeof block.CourseImage === 'object'
+            ? {
+                id: block.CourseImage.id,
+                url: block.CourseImage.url || '',
+                alt: block.CourseImage.alt,
+                width: block.CourseImage.width || 0,
+                height: block.CourseImage.height || 0,
+              }
+            : null
+             return (
+          <CourseSection
+            key={block.id}
+            data={[
+              {
+                slug: block.slug,
+                CourseImage: courseImage,
+                CoursePrice: block.CoursePrice,
+                CourseName: block.CourseName,
+                AmoutOfPeople: block.AmoutOfPeople,
+                Description: block.Description,
+                Button: block.Button,
+              },
+            ]}
+          />
+        )
+      })}
+        </>
+    )
+}
