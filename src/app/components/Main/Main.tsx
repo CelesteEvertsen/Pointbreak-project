24c97a897fcfd238244e4a3006095d0f5446e703
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-11-02 14:59:44 +0100

    page.tsx får feil melding i props

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -1,11 +1,27 @@
 import type { Main } from '@/payload-types'
 import Image from 'next/image'
 import  style  from './Main.module.css'
-
+type HomeData = {
+  id: number
+  headerImage: {
+    url: string
+    alt?: string
+    width: number
+    height: number
+  }
+  headline: string
+  subHeadline?: string
+  overlayTekstPosisjon: 'center' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+  overlayTekstFarge: string
+  underOverskrift: string
+  width: number
+  height: number
+}
 
 type Props = {
-  data: Main[]
+  data: HomeData[]
 }
+/*Med egen definer typescript så blir feil meldigen borte på alt og URL. men den dukker opp på page.tsx */
 export default async function Main({ data }: Props) {
   return (
     <main className={style.main}>
