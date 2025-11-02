489052c050ab53de7dcf26a6dbab504ecfcfefef
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-11-02 15:09:00 +0100

    style

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -2,7 +2,6 @@ import type { Main } from '@/payload-types'
 import Image from 'next/image'
 import  style  from './Main.module.css'
 
-
 type Props = {
   data: Main[]
 }
@@ -10,7 +9,7 @@ export default async function Main({ data }: Props) {
   return (
     <main className={style.main}>
       {data.map((data) => (
-        <article key={data.id} className={`${style.article} ${style[data.overlayTekstPosisjon]} `}>
+        <article key={data.id} className={`${style.article} `}>
             <div className={style.imageContainer}>
           <Image
             src={data.headerImage.url}
@@ -19,8 +18,10 @@ export default async function Main({ data }: Props) {
             width={600}
             className={style.image}
           />
+          <div className={style.overlay}>
           <h1>{data.headline}</h1>
-          <h2>{data.underOverskrift}</h2>
+         { data.underOverskrift && <h2>{data.underOverskrift}</h2>}
+          </div>
           </div>
         </article>
       ))}
