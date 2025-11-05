914ab3c93b03dd538f285221da3e726f2645b8e8
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-11-05 22:55:56 +0100

    footer

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,19 +1,18 @@
-import { headers as getHeaders } from 'next/headers.js'
 import React from 'react'
 import { getPayload } from 'payload'
 import config from '@/payload.config'
 import Main from "@/app/components/Main/Main"
 import RenderBlocks from '@/renderBlocks/RendeBlocks'
+import Footer from "@/app/components/Footer/Footer"
 
 import './styles.css'
 
 
 export default async function HomePage() {
 
-  const headers = await getHeaders()
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
+ 
   const queryResults = await payload.find({
     collection: 'main',
   })
@@ -22,31 +21,9 @@ export default async function HomePage() {
     <>
     <Main data={main}/>
     <RenderBlocks blocks={main[0]?.block}/>
+    <Footer/>
    {/*  <pre>{JSON.stringify(queryResults, null,2)}</pre>  */}
-      <div className="home">
-        <div className="content">
-          {!user && <h1>Welcome to your new project.</h1>}
-          {user && <h1>Welcome back, {user.email}</h1>}
-          <div className="links">
-            <a
-              className="admin"
-              href={payloadConfig.routes.admin}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Go to admin panel
-            </a>
-            <a
-              className="docs"
-              href="https://payloadcms.com/docs"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Documentation
-            </a>
-          </div>
-        </div>
-      </div>
+     
     </>
   )
 }
